17c9115297400652ba52e798ff1cb65e34dffe3e
Xavier Brinon <xavier@brinon.eu>
2019-04-10 12:28:11 +0100

    fixiing file upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Screen } from "./Components/Screen";
+import { Keypad } from "./Components/Keypad";
+
+export const App = () => {
+  const [history, setHistory] = useState({});
+  const [display, setDisplay] = useState("");
+
+  return (
+    <>
+      <Screen display={display} />
+      <Keypad
+        setDisplay={setDisplay}
+        display={display}
+        setHistory={setHistory}
+        history={history}
+      />
+    </>
+  );
+};
